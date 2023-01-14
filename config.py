113064955bfe76e2a8be3de3bfdb8d601ddb1e2c
hartbold <hartbold8@gmail.com>
2023-01-14 18:08:13 +0100

    abs path test

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from objects.Logs import Logs as log
 from dotenv import load_dotenv
 load_dotenv()
 
-CONF_PATH_FILE_IX = "./current_ix" # str(os.getenv("SERVER_FILE_PATH"))
-CONF_PATH_FILE_CURRENT_VIDEO_META = "./current_episode"
-CONF_PATH_FILE_VIDEO_URLS = "./all_videos.txt"
+CONF_PATH_FILE_IX = os.environ["PWD"] + "/current_ix" # str(os.getenv("SERVER_FILE_PATH"))
+CONF_PATH_FILE_CURRENT_VIDEO_META = os.environ["PWD"] + "/current_episode"
+CONF_PATH_FILE_VIDEO_URLS = os.environ["PWD"] + "/all_videos.txt"
 
 CONF_NAME_VIDEO = 'video.mp4'
 
 CONF_IX_FIRST_VIDEO = -1
 CONF_IX_FIRST_PAGE = 128
 
-CONF_PATH_FOLDER_VIDEOS = "./videos/" # str(os.getenv("SERVER_FOLDER_VID_PATH"))
-CONF_PATH_FOLDER_FRAMES = "./frames/" # str(os.getenv("SERVER_FOLDER_FRAMES_PATH"))
+CONF_PATH_FOLDER_VIDEOS = os.environ["PWD"] + "/videos/" # str(os.getenv("SERVER_FOLDER_VID_PATH"))
+CONF_PATH_FOLDER_FRAMES = os.environ["PWD"] + "/frames/" # str(os.getenv("SERVER_FOLDER_FRAMES_PATH"))
 
 CONF_TW_CONSUMER_KEY = str(os.getenv("TW_CON_KEY"))
 CONF_TW_CONSUMER_SECRET = str(os.getenv("TW_CON_KEY_SEC"))
